44541ec90b4d0a5946a236617de803fd7b794878
etto48 <minecraft.etto@gmail.com>
2021-12-13 03:05:21 +0100

    asdasdasda

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,11 +5,11 @@ if type "xrandr"; then
     PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         if [[ "${m}" = "${PRIMARY}" ]]; then
-            MONITOR=$m polybar primary & disown
+            MONITOR=$m polybar primary -r & disown
         else
-            MONITOR=$m polybar secondary & disown
+            MONITOR=$m polybar secondary -r & disown
         fi
     done
 else
-    polybar primary & disown
+    polybar primary -r & disown
 fi
