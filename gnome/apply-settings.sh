f87be6ad1da2d5fab3d894d0bc5ed8c66c7c675d
Etto48 <ettore.ricci.coding@gmail.com>
2025-06-19 17:36:45 +0200

    added enable extensions

--- gnome/apply-settings.sh
+++ gnome/apply-settings.sh
@@ -1,9 +1,15 @@
+echo "Enabling extensions"
+echo "Install any missing extensions from https://extensions.gnome.org"
+gnome-extensions enable openbar@neuromorph
+gnome-extensions enable rounded-window-corners@fxgn
+gnome-extensions enable app-hider@lynith.dev
+gnome-extensions enable grand-theft-focus@zalckos.github.com
+
 echo "Applying gnome terminal settings"
 dconf load /org/gnome/terminal/ < ./gnome/gnome-terminal.cfg
 
-echo "Applying gnome dash-to-dock settings"
+echo "Applying gnome extension settings"
 dconf load /org/gnome/shell/extensions/dash-to-dock/ < ./gnome/dash-to-dock.cfg
-echo "Applying gnome openbar settings"
 dconf load /org/gnome/shell/extensions/openbar/ < ./gnome/openbar.cfg
 dconf load /org/gnome/shell/extensions/tiling-assistant/ < ./gnome/tiling-assistant.cfg
 dconf load /org/gnome/shell/extensions/rounded-window-corners-reborn/ < ./gnome/rounded-window-corners-reborn.cfg
