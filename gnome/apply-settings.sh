191b200300c5bd5aa9375b7448befcc9bb9b98ae
Etto48 <ettore.ricci.coding@gmail.com>
2025-06-19 15:34:12 +0200

    Added more plugins config

--- gnome/apply-settings.sh
+++ gnome/apply-settings.sh
@@ -5,3 +5,5 @@ echo "Applying gnome dash-to-dock settings"
 dconf load /org/gnome/shell/extensions/dash-to-dock/ < ./gnome/dash-to-dock.cfg
 echo "Applying gnome openbar settings"
 dconf load /org/gnome/shell/extensions/openbar/ < ./gnome/openbar.cfg
+dconf load /org/gnome/shell/extensions/tiling-assistant/ < ./gnome/tiling-assistant.cfg
+dconf load /org/gnome/shell/extensions/rounded-window-corners-reborn/ < ./gnome/rounded-window-corners-reborn.cfg
