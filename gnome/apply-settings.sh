3385b08842b92c5469653e06b39d8ceccf9bf3e0
Etto48 <ettore.ricci.coding@gmail.com>
2025-06-20 15:31:18 +0200

    used gbus for plugin installation

--- gnome/apply-settings.sh
+++ gnome/apply-settings.sh
@@ -2,9 +2,6 @@
 
 echo "Installing extensions"
 
-tmp_dir=$(mktemp -d)
-trap 'rm -rf "$tmp_dir"' EXIT
-
 if ! (command -v gnome-extensions &> /dev/null); then
     echo "gnome-extensions command not found. Please install GNOME Shell extensions support."
     exit 1
@@ -13,39 +10,30 @@ fi
 install_extension() {
     local extension_name="$1"
     local extension_author="$2"
-    local extension_version="$3"
-    local extension_url="https://extensions.gnome.org/extension-data/${extension_name}${extension_author}.v${extension_version}.shell-extension.zip"
-    local extension_file="${tmp_dir}/${extension_name}_${extension_author}_v${extension_version}.zip"
     local extension_id="${extension_name}@${extension_author}"
-    local extension_fancy_name="${extension_id} v${extension_version}"
-    if [ -z "$extension_name" ] || [ -z "$extension_author" ] || [ -z "$extension_version" ]; then
-        echo "Invalid parameters for install_extension: $extension_name, $extension_author, $extension_version"
-        return 1
-    fi
+
     if gnome-extensions list | grep -q "${extension_id}"; then
-        echo "Extension ${extension_name}@${extension_author} is already installed."
-        gnome-extensions enable "$extension_id"
+        echo "Extension ${extension_name} by ${extension_author} is already installed."
         return 0
     fi
-    wget -q "$extension_url" -O "$extension_file"
-    if [ $? -eq 0 ]; then
-        echo "Installing ${extension_fancy_name}"
-        if ! (gnome-extensions install "$extension_file"); then
-            echo "Failed to install ${extension_fancy_name}"
-            return 1
-        fi
-        rm "$extension_file" 
-    else
-        echo "Failed to download ${extension_fancy_name} from ${extension_url}"
+
+    if ! (gdbus call --session \
+           --dest org.gnome.Shell.Extensions \
+           --object-path /org/gnome/Shell/Extensions \
+           --method org.gnome.Shell.Extensions.InstallRemoteExtension \
+           ${extension_id} &> /dev/null); then
+        echo "Failed to install extension: ${extension_name} by ${extension_author}"
         return 1
+    else
+        echo "Successfully installed extension: ${extension_name} by ${extension_author}"
     fi
 }
 
-install_extension openbar neuromorph 43
-install_extension rounded-window-corners fxgn 12
-install_extension app-hider lynith.dev 23
-install_extension grand-theft-focus zalckos.github.com 8
-install_extension blur-my-shell aunetx 68
+install_extension openbar neuromorph
+install_extension rounded-window-corners fxgn
+install_extension app-hider lynith.dev
+install_extension grand-theft-focus zalckos.github.com
+install_extension blur-my-shell aunetx
 
 echo "Applying gnome terminal settings"
 dconf load /org/gnome/terminal/ < ./gnome/gnome-terminal.cfg
