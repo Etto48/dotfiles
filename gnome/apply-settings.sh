f73a3727ae5bdf924d91cfa0ac2b6c464c09abcf
Etto48 <ettore.ricci.coding@gmail.com>
2025-06-23 19:47:49 +0200

    added logo menu

--- gnome/apply-settings.sh
+++ gnome/apply-settings.sh
@@ -8,12 +8,10 @@ if ! (command -v gnome-extensions &> /dev/null); then
 fi
 
 install_extension() {
-    local extension_name="$1"
-    local extension_author="$2"
-    local extension_id="${extension_name}@${extension_author}"
+    local extension_id="$1"
 
     if gnome-extensions list | grep -q "${extension_id}"; then
-        echo "Extension ${extension_name} by ${extension_author} is already installed."
+        echo "Extension ${extension_id} is already installed."
         return 0
     fi
 
@@ -22,19 +20,20 @@ install_extension() {
            --object-path /org/gnome/Shell/Extensions \
            --method org.gnome.Shell.Extensions.InstallRemoteExtension \
            ${extension_id} &> /dev/null); then
-        echo "Failed to install extension: ${extension_name} by ${extension_author}"
+        echo "Failed to install extension: ${extension_id}"
         return 1
     else
-        echo "Successfully installed extension: ${extension_name} by ${extension_author}"
+        echo "Successfully installed extension: ${extension_id}"
     fi
 }
 
-install_extension openbar neuromorph
-install_extension rounded-window-corners fxgn
-install_extension app-hider lynith.dev
-install_extension grand-theft-focus zalckos.github.com
-install_extension blur-my-shell aunetx
-install_extension mediacontrols cliffniff.github.com
+install_extension openbar@neuromorph
+install_extension rounded-window-corners@fxgn
+install_extension app-hider@lynith.dev
+install_extension grand-theft-focus@zalckos.github.com
+install_extension blur-my-shell@aunetx
+install_extension mediacontrols@cliffniff.github.com
+install_extension logomenu@aryan_k
 
 echo "Applying gnome terminal settings"
 dconf load /org/gnome/terminal/ < ./gnome/gnome-terminal.cfg
@@ -46,3 +45,4 @@ dconf load /org/gnome/shell/extensions/tiling-assistant/ < ./gnome/tiling-assist
 dconf load /org/gnome/shell/extensions/rounded-window-corners-reborn/ < ./gnome/rounded-window-corners-reborn.cfg
 dconf load /org/gnome/shell/extensions/blur-my-shell/ < ./gnome/blur-my-shell.cfg
 dconf load /org/gnome/shell/extensions/mediacontrols/ < ./gnome/mediacontrols.cfg
+dconf load /org/gnome/shell/extensions/Logo-menu/ < ./gnome/Logo-menu.cfg
