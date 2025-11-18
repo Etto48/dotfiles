d7a61685f4f0b0098cd9f1c349c001107b917f77
Etto48 <ettore.ricci.coding@gmail.com>
2025-11-18 17:28:19 +0100

    fix bug in install script

--- motd/install_motd.sh
+++ motd/install_motd.sh
@@ -2,4 +2,4 @@
 
 sudo cp motd/01-custom /etc/update-motd.d/01-custom
 sudo chmod +x /etc/update-motd.d/01-custom
-sudo chmod -x /etc/update-motd.d/10-help
+sudo chmod -x /etc/update-motd.d/10-help-text
